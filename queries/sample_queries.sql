143704d063ec5e56a1f5a2bb8ab4f60caf5ea5ba
Mona Jalilvand <mona.jalilvand89@gmail.com>
2024-09-03 18:18:00 +0200

    Create sample_queries.sql
    sample queries

---
+++ queries/sample_queries.sql
@@ -0,0 +1,6 @@
+-- Query to select all employees
+SELECT * FROM employees;
+
+-- Query to insert a new employee
+INSERT INTO employees (name, position, hire_date, email)
+VALUES ('John Doe', 'Software Developer', '2024-01-15', 'john.doe@example.com');
